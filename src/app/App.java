80547ee2ed2bdaaa11a288dccb9a81172487755c
Develor <bigkulik@gmail.com>
2022-06-03 21:05:14 +0300

    Initial commit

---
+++ src/app/App.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+    static String roundBonus;
+    static String infoMerchant;
+    static String infoProduct;
+
+    public static void main(String[] args) {
+        doInputs();
+    }
+
+    private static void doInputs() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter merchant name: ");
+        merchantName = scanner.nextLine();
+        System.out.print("Enter merchant phone: ");
+        phone = scanner.nextLine();
+        System.out.print("Enter merchant email: ");
+        email = scanner.nextLine();
+        System.out.print("Enter product name: ");
+        productName = scanner.nextLine();
+        System.out.print("Enter product quantity: ");
+        quantity = scanner.nextInt();
+        System.out.print("Enter product price: ");
+        price = scanner.nextDouble();
+    }
+
+    private static void showData(String output) {
+        System.out.println(output);
+    }
+}
