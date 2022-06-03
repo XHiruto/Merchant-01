038d049ce06f4aeed3654816c9617b3262b57b52
Develor <bigkulik@gmail.com>
2022-06-03 21:19:57 +0300

    App class supplemented

--- src/app/App.java
+++ src/app/App.java
@@ -9,12 +9,15 @@ public class App {
     static String productName;
     static int quantity;
     static double price;
+    static ProductA product;
+    static MerchantA merchantA;
     static String roundBonus;
     static String infoMerchant;
     static String infoProduct;
 
     public static void main(String[] args) {
         doInputs();
+        showData(processData());
     }
 
     private static void doInputs() {
@@ -31,6 +34,16 @@ public class App {
         quantity = scanner.nextInt();
         System.out.print("Enter product price: ");
         price = scanner.nextDouble();
+    }
+
+    private static String processData() {
+        merchantA = new MerchantA(merchantName, phone, email);
+        infoMerchant = merchantA.infoMerchant();
+        product = new ProductA(productName, quantity, price);
+        infoProduct = product.infoProduct();
+        double sales = product.calcSales(quantity, price);
+        roundBonus = Rounder.roundValue(merchantA.calcBonus(sales));
+        return infoMerchant + infoProduct + "\nBonus (USD): " + roundBonus;
     }
 
     private static void showData(String output) {
